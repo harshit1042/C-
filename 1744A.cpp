ab5312421269a05c1931265ff1537214134ab53a
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2022-10-16 21:42:13 +0530

    Add files via upload

---
+++ 1744A.cpp
@@ -0,0 +1,69 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n, cnt = 0;
+//         cin >> n;
+//         int a[n];
+//         string s;
+//         // cout << s;
+//         for (i = 0; i < n; i++)
+//         {
+//             cin >> a[i];
+//             cin >> s;
+//         }
+
+//         return 0;
+//     }
+
+//     // #include <bits/stdc++.h>
+//     // using namespace std;
+
+//     // int main()
+//     // {
+//     //     int n;
+//     //     cin >> n;
+//     //     for (int i = 0; i < n; i++)
+//     //     {
+//     //         int count = 0;
+//     //         int length;
+//     //         cin >> length;
+//     //         int a[length];
+//     //         char b[length];
+//     //         for (int i = 0; i < length; i++)
+//     //         {
+//     //             cin >> a[i];
+//     //         }
+//     //         string s;
+//     //         cin >> s;
+
+//     //         for (int j = 0; j < length; j++)
+//     //         {
+//     //             for (int k = 0; k < length; k++)
+//     //             {
+//     //                 if (a[j] == a[k] && s[j] != s[k])
+//     //                 {
+//     //                     count++;
+//     //                 }
+//     //                 // cout << a[j] << " " << a[k] << " " << s[j] << " " << s[k] << " " << endl;
+//     //             }
+//     //         }
+//     //         if (count > 0)
+//     //         {
+//     //             cout << "NO" << endl;
+//     //         }
+//     //         else
+//     //         {
+//     //             cout << "YES" << endl;
+//     //         }
+
+//     //         // sort(s.begin(), s.end());
+//     //         // cout << s << endl;
+//     //     }
+
+//     //     return 0;
+//     // }
