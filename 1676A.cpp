2f4a574c85b20ec720ba3fe57822eafbd8b5c1f3
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2022-10-19 20:43:32 +0530

    Add files via upload

---
+++ 1676A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, sum = 0, sum1 = 0;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int a = s[0] - '0', b = s[1] - '0', c = s[2] - '0';
+        int x = s[3] - '0', y = s[4] - '0', z = s[5] - '0';
+        if (a + b + c == x + y + z)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
