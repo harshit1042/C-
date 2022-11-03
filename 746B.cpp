0c3f019b9d8db61a3f33f6459014ab2df12d95e0
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2022-11-03 16:27:50 +0530

    Add files via upload

---
+++ 746B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<char> v;
+    while (n != 0)
+    {
+        if (n % 2 == 1)
+        {
+            v.push_back(s[0]);
+        }
+        else
+        {
+            v.insert(v.begin(), s[0]);
+        }
+        s.erase(0, 1);
+        n = s.length();
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i];
+    }
+    return 0;
+}
