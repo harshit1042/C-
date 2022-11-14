636139ff63c1b9e1a222086fd2ad2c9fde898ec5
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2022-11-14 11:15:54 +0530

    Add files via upload

--- 474A.cpp
+++ 474A.cpp
@@ -1,24 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    int cnt=0,cnt1=0;
-    cin>>t;
-    while(t--){
-        int a;
-        cin>>a;
-        if(a>0){
-            cnt+=a;
-        }
-        else{
-            if(cnt<1){
-                cnt1++;
+
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long>
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define int3     \
+    int a, b, c; \
+    cin >> a >> b >> c
+#define int4        \
+    int a, b, c, d; \
+    cin >> a >> b >> c >> d
+#define boost ios_base::sync_with_stdio(0), cin.tie(0)
+char key[100] = "qwertyuiopasdfghjkl;zxcvbnm,./";
+
+int main()
+{
+    char ch[2];
+    cin >> ch;
+    char input[120];
+    cin >> input;
+    if (ch[0] == 'L')
+    {
+        for (int i = 0; i < strlen(input); i++)
+        {
+            for (int j = 0; j < strlen(key); j++)
+            {
+                if (input[i] == key[j])
+                {
+                    cout << key[j + 1];
+                    break;
+                }
             }
-            else{
-                cnt--;
-            }
         }
+        cout << endl;
     }
-    cout<<cnt1<<endl;
-    return 0;
+    else
+    {
+        for (int i = 0; i < strlen(input); i++)
+        {
+            for (int j = 0; j < strlen(key); j++)
+            {
+                if (input[i] == key[j])
+                {
+                    cout << key[j - 1];
+                    break;
+                }
+            }
+        }
+        cout << endl;
+    }
 }
